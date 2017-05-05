5ebded2c41bc5650315813b448e52a8df0a1d441
Luis Iván García Luna <lgarcia@vertixit.com>
2017-05-04 23:35:22 -0500

    v1 May 04
    Check Model creation.
    Migration for Model.
    Seed for users.
    DB config, Heroku config.

---
+++ database/migrations/2017_05_05_041308_create_checks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChecksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checks', function (Blueprint $table) {
+            $table->increments('id');
+            //Banco,Folio,Beneficiario,Cantidad,Fecha de Vencimiento
+            $table->string('bank');
+            $table->smallInteger('folio');
+            $table->string('recipient');
+            $table->decimal('amount',5,2);
+            $table->date('validUntil');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checks');
+    }
+}
